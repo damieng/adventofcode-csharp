e5d51a779325527f23e1f07b49b50c6ffc6dcf89
Damien Guard <damieng@gmail.com>
2021-12-02 12:00:23 +0000

    Day 1 and 2

---
+++ 2021/Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var parsedInput = File
+            .ReadAllLines("input")
+            .Select(i => int.Parse(i))
+            .ToArray();
+
+        Console.WriteLine("Part 1 = " + Part1(parsedInput));
+        Console.WriteLine("Part 2 = " + Part2(parsedInput));
+    }
+
+    static int Part1(int[] input)
+    {
+        int? previous = null;
+        int increased = 0;
+
+        foreach (var depth in input)
+        {
+            if (previous != null && depth > previous)
+                increased++;
+            previous = depth;
+        }
+
+        return increased;
+    }
+
+    static int Part2(int[] input)
+    {
+        int? previous = null;
+        int increased = 0;
+
+        for (var i = 2; i < input.Length; i++)
+        {
+            var depth = input[i - 2] + input[i - 1] + input[i];
+            if (previous != null && depth > previous)
+                increased++;
+            previous = depth;
+        }
+
+        return increased;
+    }
+}
---
+++ 2021/Day2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    enum Direction { forward, up, down };
+
+    struct Movement
+    {
+        public Direction Direction;
+        public int Quantity;
+    }
+
+    static void Main()
+    {
+        var parsedInput = File
+            .ReadAllLines("input")
+            .Select(i => i.Split(' '))
+            .Select(i => new Movement
+            {
+                Direction = (Direction)Enum.Parse(typeof(Direction), i[0]),
+                Quantity = int.Parse(i[1])
+            })
+            .ToArray();
+
+        Console.WriteLine("Part 1 = " + Part1(parsedInput));
+        Console.WriteLine("Part 2 = " + Part2(parsedInput));
+    }
+
+    static long Part1(Movement[] input)
+    {
+        long horizontal = 0;
+        long depth = 0;
+
+        foreach (var move in input)
+        {
+            switch (move.Direction)
+            {
+                case Direction.forward:
+                    horizontal += move.Quantity;
+                    break;
+                case Direction.up:
+                    depth -= move.Quantity;
+                    break;
+                case Direction.down:
+                    depth += move.Quantity;
+                    break;
+            }
+        }
+
+        return horizontal * depth;
+    }
+
+    static long Part2(Movement[] input)
+    {
+        long horizontal = 0;
+        long depth = 0;
+        long aim = 0;
+
+        foreach (var move in input)
+        {
+            switch (move.Direction)
+            {
+                case Direction.forward:
+                    horizontal += move.Quantity;
+                    depth += aim * move.Quantity;
+                    break;
+                case Direction.up:
+                    aim -= move.Quantity;
+                    break;
+                case Direction.down:
+                    aim += move.Quantity;
+                    break;
+            }
+        }
+
+        return horizontal * depth;
+    }
+}
